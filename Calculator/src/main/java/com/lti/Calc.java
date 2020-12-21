63832dabda4879c34314f8dab98a5d22798876c0
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2020-12-21 14:59:27 +0530

    added method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -6,9 +6,14 @@ public class Calc {
 	{
 		return num1+num2;
 	}
+	public static int sub(int num1,int num2)
+	{
+		return num1-num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(20,50));
+		System.out.println(" Sum =  "+ add(20,50));
+		System.out.println(" Difference =  "+ sub(200,50));
 	}
 
 }
