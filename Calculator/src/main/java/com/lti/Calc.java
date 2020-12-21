ee87d80fcd73754b85ceb9f52521439e12adefad
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2020-12-21 15:09:32 +0530

    added prod

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -10,10 +10,16 @@ public class Calc {
 	{
 		return num1-num2;
 	}
+	
+	public static int prod(int num1,int num2)
+	{
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Sum =  "+ add(20,50));
 		System.out.println(" Difference =  "+ sub(200,50));
+		System.out.println(" Prod =  "+ prod(200,50));
 	}
 
 }
